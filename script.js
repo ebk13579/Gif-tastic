e0e4f5962da6f2e293304afa9381f2873208ba58
ebk13579 <evan.kozierachi@gmail.com>
2018-10-26 17:18:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+
+    var topics = ["Eric Cartman", "Stan Marsh", "Kyle Broflovski", "Kenny McCormick", "Butters Stotch",
+        "Wendy Testaburger", "Bebe Stevens", "Craig Tucker", "Randy Marsh", "Sheila Broflovski"];
+
+    const apiKey = "&api_key=d9OkXAN8QyxAaCPzIFjlHfNjxAKywCGy"
+
+    function displayInfo() {
+        $("#images").empty();
+        var character = $(this).attr("data-name");
+        var api = "https://api.giphy.com/v1/gifs/search?q=" + character + apiKey;
+        $.ajax({
+            url: api,
+            method: "GET"
+        }).done(function (response) {
+            var results = response.data;
+            for (var i = 0; i < 12; i++) {
+                var gifDiv = $("<div class='item'>");
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var charImage = $("<img class='pause'>");
+                charImage.attr({
+                    src: results[i].images.fixed_height_still.url,
+                    "data-still": results[i].images.fixed_height_still.url,
+                    "data-animate": results[i].images.fixed_height.url,
+                    "data-state": "still",
+                });
+                gifDiv.append(p);
+                gifDiv.append(charImage);
+                gifDiv.addClass("gifs");
+                $("#images").prepend(gifDiv);
+            }
+            $(".pause").on("click", function () {
+                var state = $(this).attr("data-state");
+                if (state == "still") {
+                    $(this).attr("src", $(this).data("animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).data("still"));
+                    $(this).attr("data-state", "still");
+                }
+            })
+        })
+    }
+    function renderButtons() {
+        $("#topics").empty();
+        $.each(topics, function (index, element) {
+            var button = $("<button/>").addClass("character").attr("data-name", element).text(element);
+            $("#topics").append(button);
+        });
+    };
+    $("#submit").on("click", function () {
+        var character = $("#newButton").val().trim();
+
+        if (character !== "") {
+            topics.push(character);
+            renderButtons();
+        }
+
+        $("#newButton").val("");
+        return false;
+    })
+    $(document).on("click", ".character", displayInfo);
+    renderButtons();
+});
